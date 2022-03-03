ce5f39ebfd54a177fce4ad778f7895a2697b1107
sandralilja <51739216+sandralilja@users.noreply.github.com>
2022-03-03 16:04:38 +0100

    Update main_post_IPA.R

--- bin/main_post_IPA.R
+++ bin/main_post_IPA.R
@@ -7,6 +7,7 @@ source('MNM_construction.R')
 # MNM_construction('data/IPA_UR-prediction', 
 #                  'data/DEGS_with_Monocle/Monocle_out_withFCs', 
 #                  'data/Multicellular_Network_Models')
+
 ### MNM costruction, example data
 MNM_construction('../example_data/IPA_UR-prediction',
                  '../example_data/DEGs',
@@ -17,6 +18,7 @@ MNM_construction('../example_data/IPA_UR-prediction',
 ### UR prioritization
 source('UR_ranking.R')
 # UR_ranking('data/IPA_UR-prediction', 'data/UR-rank')
+
 ### UR prioritization, example data
 UR_ranking('../example_data/IPA_UR-prediction', 'data/UR-rank')
 
