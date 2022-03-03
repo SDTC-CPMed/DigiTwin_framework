eebbc29a810c9015ea1fd7c9e00cca1f48aa62a2
sandralilja <51739216+sandralilja@users.noreply.github.com>
2022-03-03 13:41:07 +0100

    Update main_post_IPA.R

--- bin/main_post_IPA.R
+++ bin/main_post_IPA.R
@@ -7,25 +7,15 @@ source('MNM_construction.R')
 MNM_construction('data/IPA_UR-prediction', 
                  'data/DEGS_with_Monocle/Monocle_out_withFCs', 
                  'data/Multicellular_Network_Models')
+### UR prioritization, specific time point
 # MNM_construction('data/IPA_UR-prediction', 
 #                  'data/DEGS_with_Monocle/Monocle_out_withFCs', 
 #                  'data/Multicellular_Network_Models',
 #                  time_points = '3D')
 
+
 ### UR prioritization
 source('UR_ranking.R')
 UR_ranking('data/IPA_UR-prediction', 'data/UR-rank')
 
-
-# 
-# ## Test function
-# ### UR prioritization
-# source('scripts/MNM_construction.R')
-# MNM_construction('IPA_output/IPA_output', 
-#                  '/data/sharedData/SAR_allergen_challenge_timeseries/SAR_allergic_vs_healthy_study/MONOCLE/data/DEGS_with_Monocle/Monocle_out/withFCs_timepointX_Sick_vs_Healthy', 
-#                  'example_data/test')
-# 
-# ### UR prioritization
-# source('scripts/UR_ranking.R')
-# UR_ranking('IPA_output/IPA_output', 'example_data/test')
 
